f0ecdf22d8c8a29b3e238eeb1d7a122db339b8b2
Carolina Bonturi <carolina.bonturi@nubank.com.br>
2020-04-25 16:03:05 -0300

    Include two cards in a column

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,23 @@ void main() => runApp(MaterialApp(
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
         ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.0'),
+                subtitle: Text('1000'),
+              )
+            ),
+          ],
+        ),
       ),
     ));
