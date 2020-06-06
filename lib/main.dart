b60802a0caaf22e53730b7a0228abe066e57276f
Carolina Bonturi <carolina.bonturi@nubank.com.br>
2020-06-06 01:11:55 -0300

    Create Transfer in form

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,58 @@ class BytebankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Criando transferência'),),
-        body: Text('pitoco'));
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                final double valor = double.tryParse(_controladorCampoValor.text);
+                final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+
+                if (valor != null && numeroConta != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('$transferenciaCriada');
+                }
+              },
+              child: Text('Confirmar'),
+            )
+          ],
+        ));
   }
 }
 
@@ -62,4 +109,9 @@ class Transferencia {
   final int _numeroConta;
 
   Transferencia(this._valor, this._numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{_valor: $_valor, _numeroConta: $_numeroConta}';
+  }
 }
